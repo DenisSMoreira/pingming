861f7e918c9284fb72182d08e64db638d4c71229
denis.soares.moreira@gmail.com <denis.soares.moreira@gmail.com@54143b72-e550-6a4a-4a9d-ec155d3950a6>
2012-06-09 19:53:17 +0000

    Mudanças

---
+++ nokys/Login-ear/Login-war/src/java/web/login/autenticacao/view/redirecionamento.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.login.autenticacao.view;
+
+
+
+public class Redirecionamento {
+    
+    public String retornar(){
+        return "autenticacao";
+    }
+}
